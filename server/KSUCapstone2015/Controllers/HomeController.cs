d3d7ee83faf972569674b56c030f76161b16f79b
Evun <Evun>
2015-09-07 10:57:40 -0400

    Removed, Contact page and link to Contact page, deleted footer from layout, deleted html from index and about views

--- server/KSUCapstone2015/Controllers/HomeController.cs
+++ server/KSUCapstone2015/Controllers/HomeController.cs
@@ -15,15 +15,6 @@ namespace KSUCapstone2015.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
