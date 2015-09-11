2d0ad7318a2e316bb56b92e492a4f597605f0b78
ayerman <ayerman@kent.edu>
2015-09-11 10:05:55 -0400

    add map toggle button between 50% and 100%

--- server/KSUCapstone2015/App_Start/BundleConfig.cs
+++ server/KSUCapstone2015/App_Start/BundleConfig.cs
@@ -14,6 +14,8 @@ namespace KSUCapstone2015
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/MapInterface").Include("~/Scripts/MapInterface.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ server/KSUCapstone2015/Scripts/MapInterface.js
@@ -0,0 +1,15 @@
+﻿var com = com || {};
+com.capstone = {};
+com.capstone.mapStateOpen = true;
+
+$(document).ready(function () {
+    $('#btnReport').on("click", function () {
+        var width = "100%";
+        if (com.capstone.mapStateOpen) {
+            width = "50%";
+        }
+        com.capstone.mapStateOpen = !com.capstone.mapStateOpen;
+        console.log(width);
+        $('#map').animate({ "width" : width }, 1000);
+    });
+});
