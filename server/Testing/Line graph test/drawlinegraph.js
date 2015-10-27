fb4391daebb8212bd14fb328e99beb4b209c3c12
pcollin6 <pcollin6@kent.edu>
2015-10-27 11:28:29 -0400

    Linegraph function almost working

--- server/Testing/Line graph test/drawlinegraph.js
+++ server/Testing/Line graph test/drawlinegraph.js
@@ -1,39 +1,32 @@
-function BuildLineChart(Title, subtitle, Axis1Title, Axis2Title, data) {
+function BuildLineChart(Title, subtitle, Axis1Title, Axis2Title, dataValues) {
        var chart = new CanvasJS.Chart("chartContainer",
     {
         title:{
-        text: "Earthquakes - per month"
+        text: Title
       },
+	  subtitles:[
+      {
+            text: subtitle,
+            fontSize: 15
+      }
+      ],
       animationEnabled: true,
       axisX: {
+		title: Axis1Title,
         valueFormatString: "MMM",
         interval:1,
         intervalType: "month"
         
       },
       axisY:{
+		title: Axis2Title,
         includeZero: false
         
       },
       data: [
       {        
-        type: "line",
-        //lineThickness: 3,        
-        dataPoints: [
-        { x: new Date(2012, 00, 1), y: 450 },
-        { x: new Date(2012, 01, 1), y: 414},
-        { x: new Date(2012, 02, 1), y: 520, indexLabel: "highest",markerColor: "red", markerType: "triangle"},
-        { x: new Date(2012, 03, 1), y: 460 },
-        { x: new Date(2012, 04, 1), y: 450 },
-        { x: new Date(2012, 05, 1), y: 500 },
-        { x: new Date(2012, 06, 1), y: 480 },
-        { x: new Date(2012, 07, 1), y: 480 },
-        { x: new Date(2012, 08, 1), y: 410 , indexLabel: "lowest",markerColor: "DarkSlateGrey", markerType: "cross"},
-        { x: new Date(2012, 09, 1), y: 500 },
-        { x: new Date(2012, 10, 1), y: 480 },
-        { x: new Date(2012, 11, 1), y: 510 }
-        
-        ]
+        type: "line",        
+        dataPoints: {label: 1, y: dataValues}
       }
       
       
