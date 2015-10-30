1b4bc80b990d09630b2cae64b49763720dd0d7e3
ayerman <ayerman@kent.edu>
2015-10-30 00:16:41 -0400

    update chart
    update chart based on type

--- server/KSUCapstone2015/Scripts/ReportController.js
+++ server/KSUCapstone2015/Scripts/ReportController.js
@@ -10,61 +10,95 @@ com.capstone.ReportController = function (reportid) {
 
     this.chart = null;
     this.dataPoints = [];
+    this.title = "Average Speed";
+    this.subTitle = "In mph";
+    this.type = "splineArea";
+    this.xTitle = "Trip";
+    this.yTitle = "Speed";
 
 
     this.initChart = function () {
         self.chart = new CanvasJS.Chart("chartContainer",
-        {
-            theme: "theme2",
-            title: {
-                text: "Average Speed"
-            },
-            subtitles:[
-            {
-                text: "In mph",
-                fontSize: 15
-            }
-            ],
-            animationEnabled: true,
-            axisX: {
-                title: "Trip",
-                interval: 0,
-                labelFontColor: "white",
-                tickColor: "white"
-            },
-            axisY: {
-                title: "Speed",
-                suffix: " MPH",
-                viewportMaximum: 65
-            },
-            data: [
-                    {
-                        type: "splineArea",
-                        showInLegend: false,
-                        lineThickness: 2,
-                        markerType: "square",
-                        color: "#008000",
-                        toolTipContent: "Speed: {y}mph",
-                        dataPoints: self.dataPoints
-                    }
+       {
+           theme: "theme2",
+           title: {
+               text: self.title
+           },
+           subtitles: [
+           {
+               text: self.subTitle,
+               fontSize: 15
+           }
+           ],
+           animationEnabled: true,
+           axisX: {
+               title: self.xTitle,
+               interval: 0,
+               labelFontColor: "white",
+               tickColor: "white"
+           },
+           axisY: {
+               title: self.yTitle,
+               suffix: " MPH",
+               viewportMaximum: 65
+           },
+           data: [
+                   {
+                       type: self.type,
+                       showInLegend: false,
+                       lineThickness: 2,
+                       markerType: "square",
+                       color: "#008000",
+                       toolTipContent: "Speed: {y}mph",
+                       dataPoints: self.dataPoints
+                   }
 
-            ]
-        });
-        setInterval(function () { self.chart.render(); }, 1000);
+           ]
+       });
+       setInterval(function () { self.chart.render(); }, 1000);
     };
 
     this.updateChart = function (activeMapQuery) {
-        var counter = 1;
-        activeMapQuery.QueryResults.forEach(function (result) {
-                       var resultSpeed = Math.ceil(result.Distance / (result.Duration / 3600));
-                       if (resultSpeed < 75 && resultSpeed > 0) {
-                               self.dataPoints.push({ label: counter, y: resultSpeed });
-                               ++counter;
-                           }
-        });
+        if (self.type == "pie") {
+            this.pieChart("Average Passengers", activeMapQuery);
+        } else if (self.type == "splineArea") {
+            this.lineGraph(self.title, self.subTitle, self.xTitle, self.yTitle, activeMapQuery);
+        }
 
         self.chart.render();
     };
+
+    this.lineGraph = function (title, subtitle, xTitle, yTitle, Data) {
+        var counter = 1;
+        self.title = title;
+        self.subTitle = subtitle;
+        self.xTitle = xTitle;
+        self.yTitle = yTitle;
+        Data.QueryResults.forEach(function (result) {
+            var resultSpeed = Math.ceil(result.Distance / (result.Duration / 3600));
+            if (resultSpeed < 75 && resultSpeed > 0) {
+                self.dataPoints.push({ label: counter, y: resultSpeed });
+                ++counter;
+            }
+        });
+    }
+
+
+    this.pieChart = function (title,yData) {
+        self.title = title;
+        var counter = 1;
+        yData.QueryResults.forEach(function (result) {
+            self.dataPoints.push({ label: counter, y: result.Passengers });
+        });
+    }
+
+    this.clearChart = function () {
+        var length = self.dataPoints.length
+        for (var i = 0; i < length; i++){
+            self.dataPoints.pop();
+        }
+        self.chart.render();
+    }
 
     this.initChart();
 }
